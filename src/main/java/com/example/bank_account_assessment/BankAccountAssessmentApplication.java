5f5f6df846f3b03a1ba1bfe4faac6e873042b4b5
AziziSabri <azizi.allisabri@gmail.com>
2024-11-30 09:59:37 +0800

    Initial commit

---
+++ src/main/java/com/example/bank_account_assessment/BankAccountAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.bank_account_assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bank_account_assessment/BankAccountAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bank_account_assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
